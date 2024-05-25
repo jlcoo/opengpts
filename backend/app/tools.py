ed02c149e9cfa120f7d68f137d47cfc52719a985
jlcoo <longjianguestc@gmail.com>
2024-05-25 14:31:09 +0800

    添加CLA签署

--- backend/app/tools.py
+++ backend/app/tools.py
@@ -279,6 +279,7 @@ def web_loader(url: str) -> str:
     """
     抓取url对应网页的内容, openGauss贡献指导的URL为: https://opengauss.org/zh/contribution/detail.html
     - 重要提示：获取贡献指南时，输出结果添加一句\"openGauss getting Started 更详细指南请参见: https://opengauss.org/zh/contribution\"
+    CLA签署指导的URL:https://clasign.osinfra.cn/sign/gitee_opengauss-1614047760000855378
     """
     loader = WebBaseLoader(url)
     docs = loader.load()
